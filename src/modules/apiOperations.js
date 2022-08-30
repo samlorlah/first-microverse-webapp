253cb846a402aeb4d7e4f5891e1899f351733270
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-08-30 20:08:32 +0100

    update style to have liked class
    Correct all linters errors

--- src/modules/apiOperations.js
+++ src/modules/apiOperations.js
@@ -1,20 +1,20 @@
 import { SHOW_BASE_URL, INVOLVEMENT_API_BASE_URL } from './default.js';
 
 export const getShows = async () => {
-  const res = await fetch(`${SHOW_BASE_URL}/shows`, {method: 'GET'});
+  const res = await fetch(`${SHOW_BASE_URL}/shows`, { method: 'GET' });
   const shows = await res.json();
   return shows;
-}
+};
 
 export const getLikes = async () => {
   const res = await fetch(`${INVOLVEMENT_API_BASE_URL}/likes`, { method: 'GET' });
   const likes = await res.json();
   return likes;
-}
+};
 
-export const likeShow = async (showId) => {
+export const likeShowOps = async (showId) => {
   const formData = {
-    item_id: showId
+    item_id: showId,
   };
 
   const res = await fetch(`${INVOLVEMENT_API_BASE_URL}/likes`, {
@@ -25,4 +25,4 @@ export const likeShow = async (showId) => {
     body: JSON.stringify(formData),
   });
   return res;
-}
+};
--- src/modules/tvShow.js
+++ src/modules/tvShow.js
@@ -1,10 +1,12 @@
-import {getShows, getLikes, likeShow} from './apiOperations.js';
-
+import { getShows, getLikes, likeShowOps } from './apiOperations.js';
 
 export default class TvShows {
   displayCard(show, like) {
-    let like_count = 0;
-    like.length > 0 ? like_count = like[0].likes : 0;
+    let likeCount = 0;
+    if (like.length > 0) {
+      likeCount = like[0].likes;
+    }
+
     const card = document.createRange().createContextualFragment(`
     <div class="card">
       <img src="${show.image.medium}" alt="${show.name}" class="card-img">
@@ -14,51 +16,52 @@ export default class TvShows {
       <span data-id="${show.id}" id="like_btn_${show.id}">
       <i class="fa-regular fa-heart fa-2x"></i>
       </span>
-      <p id="like_count_displayed_${show.id}">${like_count} ${like_count > 1 ? 'Likes' : 'Like'}</p>
+      <p id="likeCount_displayed_${show.id}">${likeCount} ${likeCount > 1 ? 'Likes' : 'Like'}</p>
       </div>
       </div>
       <div class="card-btns">
-      <button data-id="${
-        show.id
-      }" class="comment-btn" type="button">Comments</button>
+      <button data-id="${show.id}" class="comment-btn" type="button">Comments</button>
       </div>
       </div>`);
-      
-    const card_container = document.querySelector('.main-container');
-    card_container.append(card);
-    
+
+    const cardContainer = document.querySelector('.main-container');
+    cardContainer.append(card);
+
     const likeBtnEvent = document.querySelector(`#like_btn_${show.id}`);
-    const likeCountDisplayed = document.querySelector(`#like_count_displayed_${show.id}`);
+    const likeCountDisplayed = document.querySelector(`#likeCount_displayed_${show.id}`);
     likeBtnEvent.addEventListener('click', async () => {
       const id = likeBtnEvent.getAttribute('data-id');
       const child = likeBtnEvent.childNodes;
-      child[1].classList.add("liked");
+      child[1].classList.add('liked');
       await this.likeShow(id);
-      let currentLike = parseInt(likeCountDisplayed.innerHTML.split(" ")[0]);
+      const currentLike = parseInt(likeCountDisplayed.innerHTML.split(' ')[0], 10);
       const newLike = currentLike + 1;
-      if(newLike > 1)
-      {
-        likeCountDisplayed.innerHTML = `${newLike} Likes`
-      }else {
+      if (newLike > 1) {
+        likeCountDisplayed.innerHTML = `${newLike} Likes`;
+      } else {
         likeCountDisplayed.innerHTML = `${newLike} Like`;
       }
       child[1].classList.remove('liked');
     });
   }
 
-  async getShows(){
+  async getShows() {
     const shows = await getShows();
     const likes = await getLikes();
     const showCount = document.querySelector('#show_count');
     showCount.innerHTML = shows.length;
-    shows.forEach((show, index) => {
-      const like = likes.filter((like) => {if(like.item_id == show.id) {return like}});
+    shows.forEach((show) => {
+      const like = likes.filter((like) => {
+        if (parseInt(like.item_id, 10) === parseInt(show.id, 10)) {
+          return like;
+        }
+        return false;
+      });
       this.displayCard(show, like);
     });
   }
 
   async likeShow(showId) {
-    const res = await likeShow(showId);
+    this.res = await likeShowOps(showId);
   }
 }
-
